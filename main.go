43c7e51bc7dde6d1b3278e60e8aa785cd7f70ac3
samarqandi <san4o.original@gmail.com>
2023-06-09 01:00:00 +0500

    add: new dependencies to main func

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/samarqandi/blog-gin/api/controller"
+	"github.com/samarqandi/blog-gin/api/repository"
+	"github.com/samarqandi/blog-gin/api/routes"
+	"github.com/samarqandi/blog-gin/api/service"
 	"github.com/samarqandi/blog-gin/infrastructure"
+	"github.com/samarqandi/blog-gin/models"
 )
 
-func main() {
-	router := gin.Default()
-	
+func init() {
+	infrastructure.LoadEnv()
+}
 
-	router.GET("/", func(ctx *gin.Context) {
-		infrastructure.LoadEnv()
-		infrastructure.NewDatabase()
-		ctx.JSON(http.StatusOK, gin.H{"data": "Hello, Gin!"})
-	})
-	router.Run(":8000")
+func main() {
+
+	router := infrastructure.NewGinRouter()
+	db := infrastructure.NewDatabase()
+	postRepository := repository.NewPostRepository(db)
+	postService := service.NewPostService(postRepository)
+	postController := controller.NewPostController(postService)
+	postRoute := routes.NewPostRoute(postController, router)
+	postRoute.Setup()
+
+	db.DB.AutoMigrate(&models.Post{})
+	router.Gin.Run(":8000")
+
 }
