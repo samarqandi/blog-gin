0a536ce489cedf3cc2e73baa81ab8bf3bdb6e0f1
samarqandi <san4o.original@gmail.com>
2023-06-08 10:47:13 +0500

    add: env variables and sql settigs

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/samarqandi/blog-gin/infrastructure"
 )
 
 func main() {
 	router := gin.Default()
+
 	router.GET("/", func(ctx *gin.Context) {
+		infrastructure.LoadEnv()
+		infrastructure.NewDatabase()
 		ctx.JSON(http.StatusOK, gin.H{"data": "Hello, Gin!"})
 	})
 	router.Run(":8000")
